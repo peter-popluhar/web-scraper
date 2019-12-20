346b7c461576fdcc9821859eba623e5875fa5bd6
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-12-20 16:33:12 +0100

    progress

---
+++ output.js
@@ -0,0 +1,60 @@
+{
+  "metaTagsData": {
+    "title": "Social Media Analytics and Reporting | Socialbakers",
+    "description": "Measure your performance with cutting edge social media analytics tools. Monitor your results with automated visual reports to perfect your social media management.",
+    "ogTitle": "Turn Your Challenge into a Solution : Advanced Social Media Analytics Tools",
+    "ogDescription": "Measure your social media performance over the course of time, benchmark yourself against your competition, evaluate the effectiveness of your content strategy and more.",
+    "ogImage": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/og-images/OG_feature_page_Social_Media_Analytics.png",
+    "ogUrl": "https://www.socialbakers.com/feature/social-media-analytics",
+    "shareTwitterTitle": "Turn Your Challenge into a Solution : Advanced Social Media Analytics Tools",
+    "shareTwitterText": "Measure your performance with cutting edge social media analytics tools. Monitor your results with automated visual reports to perfect your social media management."
+  },
+  "heroBannerData": {
+    "platformTag": {
+      "name": "",
+      "text": "Social Media Analytics",
+      "src": ""
+    },
+    "title": "Make Accurate Decisions With Social Media Analytics",
+    "subtitle": "Measure the impact of your strategy with social media analytics. Leverage actionable insights to drive real business results and easily share data across the business.",
+    "buttons": {
+      "leadForm": "Request a Demo",
+      "trialForm": "Try It for Free"
+    },
+    "img": {
+      "xs": {
+        "webP": {
+          "normal": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-extra-small.webp",
+          "retina": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-extra-small@2x.webp"
+        }
+      },
+      "md": {
+        "webP": {
+          "normal": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-small.webp",
+          "retina": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-small@2x.webp"
+        }
+      },
+      "lg": {
+        "webP": {
+          "normal": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-medium.webp",
+          "retina": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero-medium@2x.webp"
+        }
+      },
+      "xl": {
+        "webP": {
+          "normal": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero.webp",
+          "retina": "https://www.socialbakers.com/www/storage/www/pages/feature-pages/social-media-analytics/hero@2x.webp"
+        },
+        "png": {
+          "normal": "",
+          "retina": ""
+        }
+      },
+      "alt": "Social Media Analytics"
+    }
+  },
+  "subtitleData": {
+    "title": "Social Media Analytics",
+    "content": "Access competitive insights and benchmarks to outperform your competitors. Uncover your company's strengths and weaknesses with social media analytics you can use at scale. Report on your results and analysis at the click of a button."
+  }
+}
--- scraper.js
+++ scraper.js
@@ -1,34 +1,130 @@
+const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const page = '/feature/social-media-collaboration'
+const page = '/feature/social-media-analytics'
 
 const url = `https://www.socialbakers.com/${page}`;
 
+let metaTagsData = {}
+let heroBannerData = {}
+let subtitleData = {}
+let copy = {}
+
+function splitSrcetToArray(str) {
+    let arr = str.split(' ');
+    return arr
+}
+
+// TODO get non retina => chack if item doesnt have '@2x' and its length is more than for ex.: 5
+const filterItems = (arr, query) => {
+    return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+};
+
+const sanitizeInput = (input) => {
+    return input.trim().replace(/\s+/g, ' ')
+}
+
 axios(url)
     .then(response => {
-        const copy = [];
         const html = response.data;
         const $ = cheerio.load(html);
-        const bannerHeader = $('.feature-page__banner-title').text().trim()
-        const bannerPerex = $('.feature-page__banner-perex').text().trim()
-        //const test = $('.feature-page__features-perex').eq(3).html().trim().replace(/\s+/g, ' ');
-        const test = $('.feature-page__features-perex').each((i, el) => {
-            copy.push({
-                perex: $(el).html().trim().replace(/\s+/g, ' ')
-            })
 
+        const bannerImageSelector = '.feature-page__banner picture source';
+
+        metaTagsData = {
+            title: sanitizeInput($("title").text()),
+            description: sanitizeInput($('meta[name=description]').attr("content")),
+            ogTitle: sanitizeInput($('meta[property="og:title"]').attr("content")),
+            ogDescription: sanitizeInput($('meta[property="og:description"]').attr("content")),
+            ogImage: sanitizeInput($('meta[property="og:image"]').attr("content")),
+            ogUrl: sanitizeInput($('meta[property="og:url"]').attr("content")),
+            shareTwitterTitle: sanitizeInput($('meta[name="twitter:title"]').attr("content")),
+            shareTwitterText: sanitizeInput($('meta[name="twitter:description"]').attr("content")),
+        };
+
+        heroBannerData = {
+            platformTag: {
+                name: '',
+                text: sanitizeInput($('.breadcrumb a').text()),
+                src: '',
+            },
+            title: sanitizeInput($('.feature-page__banner-title').text()),
+            subtitle: sanitizeInput($('.feature-page__banner-perex').text()),
+            buttons: {
+                leadForm: sanitizeInput($('.feature-page__banner .banner-cta__item:first-child').text()),
+                trialForm: sanitizeInput($('.feature-page__banner .banner-cta__item:last-child').text()),
+            },
+            img: {
+                xs: {
+                    webP: {
+                        normal: splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(1)`)[0].attribs.srcset))[0],
+                        retina: filterItems(
+                            splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(1)`)[0].attribs.srcset)), '@2x'
+                        )[0],
+                    },
+                },
+                md: {
+                    webP: {
+                        normal: splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(2)`)[0].attribs.srcset))[0],
+                        retina: filterItems(
+                            splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(2)`)[0].attribs.srcset)), '@2x'
+                        )[0],
+                    },
+                },
+                lg: {
+                    webP: {
+                        normal: splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(3)`)[0].attribs.srcset))[0],
+                        retina: filterItems(
+                            splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(3)`)[0].attribs.srcset)), '@2x'
+                        )[0],
+                    },
+                },
+                xl: {
+                    webP: {
+                        normal: splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(4)`)[0].attribs.srcset))[0],
+                        retina: filterItems(
+                            splitSrcetToArray(sanitizeInput($(`${bannerImageSelector}:nth-of-type(4)`)[0].attribs.srcset)), '@2x'
+                        )[0],
+                    },
+                    png: {
+                        normal: '',
+                        retina: '',
+                    },
+                },
+                alt: $('.feature-page__banner-image').attr("alt"),
+            },
+        };
+
+        subtitleData = {
+            title: sanitizeInput($('.feature-page__description-title').text()),
+            content: sanitizeInput($('.feature-page__description-perex').text()),
+        };
+
+        copy = {
+            metaTagsData: metaTagsData,
+            heroBannerData: heroBannerData,
+            subtitleData: subtitleData,
+        };
+
+
+
+        console.log(
+            copy
+        )
+
+        let jsonContent = JSON.stringify(copy, undefined, 2);
+        fs.writeFile("output.js", jsonContent, 'utf8', function (err) {
+            if (err) {
+                console.log("An error occured while writing JSON Object to File.");
+                return console.log(err);
+            }
+
+            console.log("JSON file has been saved.");
         });
 
 
-        //console.log(bannerHeader);
 
-
-            //copy.bannerHeader = bannerHeader;
-            //copy.bannerPerex = bannerPerex;
-            //copy.test = test;
-
-
-        console.log(copy);
     })
     .catch(console.error);
+

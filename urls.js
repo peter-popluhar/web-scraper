3a568271e8c1067144ab77422e5a824dfe40ae36
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-12-27 15:45:04 +0100

    urls 2

--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
-export const urlGood = [
+const urlGood = [
 	"audience-analytics",
 	"audience-segmentation",
 	"content-scheduling-and-publishing",
@@ -18,34 +18,43 @@ export const urlGood = [
 	"social-media-automation",
 	"social-media-collaboration",
 	"social-media-content-curation",
+	"social-media-content-insights",
 	"social-media-content-planning-tool",
 	"social-media-content-workflow",
 	"social-media-dashboards-and-reporting",
 	"social-media-listening",
 	"social-media-optimization",
-	"social-media-performance-marketing",
 	"social-media-reporting-tools",
 	"social-media-scheduling-tools",
 	"social-media-video-analytics",
 ]
 
-export const urlBad = [
-	"live-video-analytics",
-	"social-media-content-insights",
-	// error because of no feature in url
-	"social-media-benchmarks",
+// in index.js change subdomain = `/feature/${alias}` to subdomain = `${alias}` because of url not consistent
+const urlBad = [
 	"smart-content-publishing-at-scale",
 	"easier-social-media-collaboration",
 	"the-most-reliable-marketing-platform",
 	"audiences-and-influencers",
-	// error url
+]
+
+// mess in banner images/formats
+const missingWebPInBanner = [
+	"live-video-analytics",
+	"social-media-content-insights",
+	"social-media-content-insights",
+	"social-media-benchmarks",
+]
+
+// unique design
+const unableToParse = [
 	"whats-new/youtube-influencers",
 ]
 
 
+
 export const urlAll = [
 	...urlGood,
-	// ...urlBad
-	// "paid-social-media-marketing",
-	// "social-media-performance-marketing",
+	// ...urlBad,
+	// ...missingWebPInBanner,
+	// ...unableToParse
 ]
